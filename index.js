3d9738f96f4888f44457ef1754726ba493368329
Ashley Pereira <ashleypereira70@gmail.com>
2018-10-23 12:38:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,31 +1,22 @@
-var katzDeliLine = []
-// Build an empty array representing the beginning of the day 
+katzDeliLine = [];
 
 function takeANumber(katzDeliLine, name){
-     // function defined that accepts two parrameters
-    // used to push the elements to the end of the array
-   //here we are welcoming the customers at the deli with their name
-  //and informing them with the name and their number in the queue.
-  
   katzDeliLine.push(name);
   return (`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`);
 }
 
 function nowServing(katzDeli) {
-  let i = 0;
-  while (i < katzDeli.length) {
+  let i = [];
+  while (i<katzDeli.length){
     i++;
-  }
-  if (katzDeli.length === 0) {
+    if (katzDeli.length === 0) {
     return "There is nobody waiting to be served!";
   }
   else
-  return (`Currently serving ${katzDeli.shift()}.`)
+  return (`Currently serving ${katzDeli.shift()}.`);
+
+  }
 }
-  //form a function that accepts current line of people
- // using a while loop, show that 
- // say that we were serving one person in the line
-// If or else statement shows the whole line, where there is no one in the queue and then returns the else statement.
 
 function currentLine(line) {
 var theLine=[];
@@ -35,10 +26,7 @@ if(line>[]) {
     }
     return "The line is currently:" + theLine;
 }
-    else {
+    else{
       return "The line is currently empty."
     }
 }
-
-// Open a function that accepts the current line of people
-//then push everyone to the end of the line, which returns the required phrase
